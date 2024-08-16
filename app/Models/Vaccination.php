5347beb8fe7906a7464e165f9436cfcf2fcf4b78
Mattseph <105849780+Mattseph@users.noreply.github.com>
2024-08-16 10:11:37 +0800

    Add: Vaccination Model

---
+++ app/Models/Vaccination.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Vaccination extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'vaccine_status',
+        'vaccine_1',
+        'vaccine_1_date',
+        'vaccine_2',
+        'vaccine_2_date',
+        'booster_status',
+        'booster_1',
+        'booster_1_date',
+        'booster_2',
+        'booster_2_date',
+    ];
+}
