a9c23d6bc9e92f706f8cce7b97b5786d9c76041e
Mattseph <105849780+Mattseph@users.noreply.github.com>
2024-08-16 10:12:39 +0800

    Edit: Add additional fields to Resident Model

--- app/Models/Resident.php
+++ app/Models/Resident.php
@@ -10,9 +10,28 @@ class Resident extends Model
     use HasFactory;
 
     protected $fillable = [
-        'name',
-        'address',
+        'lastname',
+        'firstname',
+        'midname',
+        'suffix',
+        'sex',
+        'date_of_birth',
+        'place_of_birth',
+        'civil_status',
         'nationality',
+        'occupation',
+        'religion',
+        'blood_type',
+        'educational_attainment',
+        'phone_number',
+        'tel_number',
+        'email',
+        'purok',
+        'barangay',
+        'city',
+        'province',
+        'fourps_status',
+        'date_of_death',
         'image',
     ];
 }
