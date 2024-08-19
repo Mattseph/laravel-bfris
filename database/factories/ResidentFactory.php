2447b64bb97b22a5cd2514ae75d12da359afffd5
Mattseph <105849780+Mattseph@users.noreply.github.com>
2024-08-19 16:10:35 +0800

    Edit: Add additional columns to be filled with fake data

--- database/factories/ResidentFactory.php
+++ database/factories/ResidentFactory.php
@@ -18,10 +18,29 @@ class ResidentFactory extends Factory
     {
         return [
             //
-            'name' => fake()->name(),
-            'address' => fake()->realText(),
-            'nationality' => fake()->realText(),
-            'image' => fake()->realText()
+            'lastname' => fake()->lastName(),
+            'firstname' => fake()->firstName(),
+            'midname' => fake()->lastName(),
+            'suffix' => fake()->suffix(),
+            'sex' => fake()->suffix(),
+            'date_of_birth' => fake()->date($format = 'Y-m-d', $max = 'now'),
+            'place_of_birth' => fake()->city(),
+            'civil_status' => fake()->realText($maxNbChars = 10, $indexSize = 2),
+            'nationality' => fake()->country(),
+            'occupation' => fake()->jobTitle(),
+            'religion' => fake()->lastName(),
+            'blood_type' => fake()->jobTitle(),
+            'educational_attainment' => fake()->jobTitle(),
+            'phone_number' => fake()->unique()->phoneNumber(),
+            'tel_number' => fake()->unique()->phoneNumber(),
+            'email' => fake()->unique()->safeEmail(),
+            'purok' => fake()->streetName(),
+            'barangay' => fake()->secondaryAddress(),
+            'city' => fake()->city(),
+            'province' => fake()->state(),
+            'fourps_status' => fake()->realText($maxNbChars = 10, $indexSize = 2),
+            'date_of_death' => fake()->date($format = 'Y-m-d', $max = 'now'),
+            'image' => fake()->imageUrl($width = 640, $height = 480),
         ];
     }
 }
