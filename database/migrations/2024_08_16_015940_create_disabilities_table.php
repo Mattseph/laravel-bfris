91d37c01c5fceae03f69d297a124cb96a41ddb64
Mattseph <105849780+Mattseph@users.noreply.github.com>
2024-08-19 15:58:54 +0800

    Add: Disabilities Migration

---
+++ database/migrations/2024_08_16_015940_create_disabilities_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('disabilities', function (Blueprint $table) {
+            $table->id('disability_id'); // Primary key column
+            $table->unsignedBigInteger('resident_id'); // Define as unsigned integer
+            $table->foreign('resident_id')
+                  ->references('resident_id')
+                  ->on('residents')
+                  ->onUpdate('cascade')
+                  ->onDelete('cascade');
+            $table->string('disability_status');
+            $table->string('disability_type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('disablilities');
+    }
+};
