9b9104345edcf9cd71c658b0a89b553324eac50b
Mattseph <105849780+Mattseph@users.noreply.github.com>
2024-08-19 15:59:14 +0800

    Add: Vaccinations Migration

---
+++ database/migrations/2024_08_16_015954_create_vaccinations_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vaccinations', function (Blueprint $table) {
+            $table->id('vaccine_id');
+            $table->unsignedBigInteger('resident_id');
+            $table->foreign('resident_id')
+                ->references('resident_id')
+                ->on('residents')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->string('vaccine_status');
+            $table->string('vaccine_1')->nullable();
+            $table->date('vaccine_1_date')->nullable();
+            $table->string('vaccine_2')->nullable();
+            $table->date('vaccine_2_date')->nullable();
+            $table->string('booster_status');
+            $table->string('booster_1')->nullable();
+            $table->date('booster_1_date')->nullable();
+            $table->string('booster_2')->nullable();
+            $table->date('booster_2_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vaccinations');
+    }
+};
